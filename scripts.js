beb8f65bdb48efbcddf4c08d9125e49f82ef3549
ital01 <italolenza@gmail.com>
2024-01-18 07:29:15 -0300

    comit

--- scripts.js
+++ scripts.js
@@ -1,173 +1,62 @@
-/*!
- * Start Bootstrap - Agency v7.0.12 (https://startbootstrap.com/theme/agency)
- * Copyright 2013-2023 Start Bootstrap
- * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
- */
-//
-// Scripts
-//
+const getById = (id) => document.getElementById(id);
+const addEventListener = (element, event, handler) => element.addEventListener(event, handler);
+const toggleDisplay = (element, display) => { if (element) element.style.display = display; };
 
-document.addEventListener("DOMContentLoaded", (event) => {
-  // Navbar shrink function
-  const navbarShrink = () => {
-    const mainNavMobile = document.body.querySelector("#mainNavmob");
-    const mainNavDesktop = document.body.querySelector("#mainNavdesk");
+const formatPhoneNumber = (input) => {
+  const phoneNumber = input.value.replace(/\D/g, "");
+  const formattedPhoneNumber = [
+    phoneNumber.substring(0, 2),
+    phoneNumber.substring(2, 8) ? ` ${phoneNumber.substring(2, 8)}` : "",
+    phoneNumber.substring(8, 13) ? `-${phoneNumber.substring(8, 13)}` : ""
+  ].join("");
 
-    if (mainNavMobile && mainNavDesktop) {
-      const shouldShrink = window.scrollY !== 0;
-      mainNavMobile.classList.toggle("navbar-shrink", shouldShrink);
-      mainNavDesktop.classList.toggle("navbar-shrink", shouldShrink);
-    }
-  };
+  input.value = formattedPhoneNumber;
+};
 
-  navbarShrink();
+const overlay = getById("overlay");
+const imagem = getById("imagem");
+const header = getById("page-top");
+const botaoExibirImagem = getById("exibir-imagem");
+const fechar = getById("fechar");
 
-  document.addEventListener("scroll", navbarShrink);
+const bloquearRolagem = () => document.body.style.overflow = "hidden";
+const desbloquearRolagem = () => document.body.style.overflow = "auto";
 
-  const mainNav = document.body.querySelector("#mainNavmob, #mainNavdesk");
+const toggleNavbar = (display) => toggleDisplay(header, display);
+const setImagem = (src) => imagem.setAttribute("src", src);
+const toggleCarouselControls = (display) => {
+  const carouselControlsPrev = getById("CarouselControlPrev");
+  const carouselControlsNext = getById("CarouselControlNext");
+  toggleDisplay(carouselControlsPrev, display);
+  toggleDisplay(carouselControlsNext, display);
+};
 
-  if (mainNav) {
-    new bootstrap.ScrollSpy(document.body, {
-      target: mainNav,
-      rootMargin: "0px 0px -40%",
-    });
-  }
-})
-
-function navbarResponsive1() {
-  const navbarToggler = document.body.querySelector(".navbar-toggler");
-  const responsiveNavItems = Array.from(
-    document.querySelectorAll("#navbarResponsive .nav-link")
-  );
-
-  responsiveNavItems.forEach((responsiveNavItem) => {
-    responsiveNavItem.addEventListener("click", () => {
-      if (window.getComputedStyle(navbarToggler).display !== "none") {
-        navbarToggler.click();
-      }
-    });
-  });
-}
-
-function navbarResponsive1() {
-  const navbarToggler = document.body.querySelector(".navbar-toggler");
-  const responsiveNavItems = Array.from(
-    document.querySelectorAll("#navbarResponsive .nav-link")
-  );
-
-  responsiveNavItems.forEach((responsiveNavItem) => {
-    responsiveNavItem.addEventListener("click", () => {
-      if (window.getComputedStyle(navbarToggler).display !== "none") {
-        navbarToggler.click();
-      }
-    });
-  });
-}
-
-function navbarResponsive2() {
-  const menuItems = document.querySelectorAll(".navbar-nav .nav-link");
-  menuItems.forEach((item) => {
-    item.addEventListener("click", () => {
-      const navbarCollapse = document.querySelector(".navbar-collapse");
-      if (navbarCollapse.classList.contains("show")) {
-        navbarCollapse.classList.remove("show");
-      }
-    });
-  });
-}
-
-navbarResponsive1();
-navbarResponsive2();
-
-
-function formatPhoneNumber(input) {
-  let phoneNumber = input.value.replace(/\D/g, "");
-
-  if (phoneNumber.length > 2) {
-    phoneNumber = phoneNumber.substring(0, 2) + " " + phoneNumber.substring(2);
-  }
-  if (phoneNumber.length > 8) {
-    phoneNumber = phoneNumber.substring(0, 8) + "-" + phoneNumber.substring(8);
-  }
-  if (phoneNumber.length > 15) {
-    phoneNumber = phoneNumber.substring(0, 13);
-  }
-
-  input.value = phoneNumber;
-}
-
-const botaoExibirImagem = document.getElementById("exibir-imagem");
-const overlay = document.getElementById("overlay");
-const imagem = document.getElementById("imagem");
-const fechar = document.getElementById("fechar");
-const header = document.getElementById("page-top");
-
-function bloquearRolagem() {
-  document.body.style.overflow = "hidden";
-}
-
-function desbloquearRolagem() {
-  document.body.style.overflow = "auto";
-}
-
-function desabilitarNavbar() {
-  if (header) {
-    header.style.display = "none";
-  }
-}
-
-function habilitarNavbar() {
-  if (header) {
-    header.style.display = "block";
-  }
-}
-
-function expandirImagem() {
-  conteudo.style.height = "100vh";
-}
-
-function restaurarTamanhoImagem() {
-  conteudo.style.height = "";
-}
-
-botaoExibirImagem.addEventListener("click", function () {
-  const caminhoDaImagem = "assets/conteudo/sistemas/FolderGED.webp";
-  imagem.setAttribute("src", caminhoDaImagem);
+const showOverlay = () => {
   overlay.style.display = "block";
   overlay.classList.add("active");
-  bloquearRolagem();
-  desabilitarNavbar();
+};
 
-  const carouselIndicators = document.querySelector(
-    "#carouselsistemas .carousel-indicators"
-  );
-  carouselIndicators.style.display = "none";
-  const carouselControls = document.querySelector(
-    "#carouselsistemas .carousel-control-prev"
-  );
-  carouselControls.style.display = "none";
-  const carouselControlsNext = document.querySelector(
-    "#carouselsistemas .carousel-control-next"
-  );
-  carouselControlsNext.style.display = "none";
-});
-
-fechar.addEventListener("click", function () {
-  imagem.setAttribute("src", "");
+const hideOverlay = () => {
   overlay.style.display = "none";
   overlay.classList.remove("active");
+};
+
+const exibirImagemHandler = () => {
+  const caminhoDaImagem = "assets/conteudo/sistemas/FolderGED.webp";
+  setImagem(caminhoDaImagem);
+  showOverlay();
+  bloquearRolagem();
+  toggleNavbar("none");
+  toggleCarouselControls("none");
+};
+
+const fecharImagemHandler = () => {
+  setImagem("");
+  hideOverlay();
   desbloquearRolagem();
-  habilitarNavbar();
-  const carouselIndicators = document.querySelector(
-    "#carouselsistemas .carousel-indicators"
-  );
-  carouselIndicators.style.display = "block";
-  const carouselControls = document.querySelector(
-    "#carouselsistemas .carousel-control-prev"
-  );
-  carouselControls.style.display = "block";
-  const carouselControlsNext = document.querySelector(
-    "#carouselsistemas .carousel-control-next"
-  );
-  carouselControlsNext.style.display = "block";
-});
+  toggleNavbar("block");
+  toggleCarouselControls("block");
+};
+
+addEventListener(botaoExibirImagem, "click", exibirImagemHandler);
+addEventListener(fechar, "click", fecharImagemHandler);
