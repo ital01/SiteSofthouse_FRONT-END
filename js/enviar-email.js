6ba0f26a7803ef0607ff5b9ce8c75a20efd213e0
ital01 <italolenza@gmail.com>
2024-02-09 12:47:24 -0300

    d

--- js/enviar-email.js
+++ js/enviar-email.js
@@ -4,17 +4,21 @@ document.addEventListener('DOMContentLoaded', function () {
     submitButton.disabled = true;
 
     var form = new FormData(document.getElementById('contatoForm'));
+    var resposta = document.getElementById('resposta');
+    resposta.innerHTML = 'Enviando...';
+    resposta.className = 'text-center alert alert-info fs-5_5';
+
     var xhr = new XMLHttpRequest();
     xhr.open('POST', '', true);
     xhr.onload = function () {
       if (xhr.status === 200) {
         try {
           var resultado = JSON.parse(xhr.responseText);
-          var resposta = document.getElementById('resposta');
           resposta.innerHTML = resultado.message;
-          resposta.className = 'text-center alert';
+          resposta.className = 'text-center alert fs-5_5';
 
           if (resultado.status === 'success') {
+            resposta.classList.remove('alert-info');
             resposta.classList.add('alert-success');
             resposta.classList.add('text-center');
             document.getElementById('contatoForm').reset();
@@ -26,6 +30,7 @@ document.addEventListener('DOMContentLoaded', function () {
             }, 4000);
             grecaptcha.reset();
           } else {
+            resposta.classList.remove('alert-info');
             resposta.classList.add('alert-danger');
             resposta.classList.add('text-center');
             setTimeout(function () {
@@ -38,6 +43,7 @@ document.addEventListener('DOMContentLoaded', function () {
         } catch (e) {
           console.error('Erro ao analisar a resposta JSON:', e);
           resposta.innerHTML = 'Erro durante o processamento da resposta do servidor.';
+          resposta.classList.remove('alert-info');
           resposta.classList.add('alert-danger');
           resposta.classList.add('text-center');
 
@@ -46,8 +52,8 @@ document.addEventListener('DOMContentLoaded', function () {
         }
       } else {
         console.error('Erro durante a requisição AJAX. Status:', xhr.status);
-        var resposta = document.getElementById('resposta');
-        resposta.innerHTML = 'Erro durante a requisição AJAX. <br> Atualize a pagina e tente novamente. <br> Se o erro persistir, entre em contato com nosso suporte.';
+        resposta.innerHTML = 'Erro durante a requisição AJAX. <br> Atualize a página e tente novamente. <br> Se o erro persistir, entre em contato com nosso suporte.';
+        resposta.classList.remove('alert-info');
         resposta.classList.add('alert-danger');
         resposta.classList.add('text-center');
         document.getElementById('contatoForm').reset();
@@ -63,8 +69,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
     xhr.onerror = function () {
       console.error('Erro durante a requisição AJAX. Status:', xhr.status);
-      var resposta = document.getElementById('resposta');
-      resposta.innerHTML = 'Erro durante a requisição AJAX. <br> Atualize a pagina e tente novamente. <br> Se o erro persistir, entre em contato com nosso suporte.';
+      resposta.innerHTML = 'Erro durante a requisição AJAX. <br> Atualize a página e tente novamente. <br> Se o erro persistir, entre em contato com nosso suporte.';
+      resposta.classList.remove('alert-info');
       resposta.classList.add('alert-danger');
       resposta.classList.add('text-center');
       document.getElementById('contatoForm').reset();
